f0ca1f9c3918330aacf87e3735d0ce916a25991d
unknown <tofolo17@gmail.com>
2024-03-10 21:08:36 -0300

    Adiciona opção de atualizar conta

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
+STAGE = "PRODUCAO"  # DESENVOLVIMENTO / PRODUCAO
+
 
 def env_settings(STAGE):
     if STAGE == "DESENVOLVIMENTO":
@@ -11,24 +13,39 @@ def env_settings(STAGE):
 
 
 def main():
+    # Check for updates
+    os.system('git pull')
+
     # Install the required packages
     os.system('pip install -r requirements.txt')
     print('Packages installed.')
 
     # Requests username and password from the user
-    username = input('Enter your username: ')
-    password = input('Enter your password: ')
+    r = input('Do you want to update your account? [Y/N]: ')
+    if r.lower() == 'y':
+        username = input('Enter your username: ')
+        password = input('Enter your password: ')
+    else:
+        # Get already saved settings
+        env_file = env_settings(STAGE)
+        with open(env_file, 'r') as file:
+            lines = file.readlines()
+            username = lines[0].split('=')[1].strip()
+            password = lines[1].split('=')[1].strip()
 
     # PRINTAR OPÇÕES DE MONEY POSSÍVEIS (ABRIR JOGO)
 
     # Requests how much money the user wants to bet (minimum R$10 / R$5 each column)
-    money = input('Enter the amount of money you want to bet (minimum R$10 / R$5 each column): ')
+    money = input(
+        'Enter the amount of money you want to bet: R$'
+    )
 
     # Get settings
-    env_file = env_settings("PRODUCAO")
+    env_file = env_settings(STAGE)
 
     # Save the username and password in a config environment file
     with open(env_file, 'w') as file:
+
         file.write(f'USER={username}\n')
         file.write(f'PASSWORD={password}\n')
         file.write(f'MONEY={money}\n')
